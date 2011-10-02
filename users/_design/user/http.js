e9caf6a848467e148d838cf9f40685cf25648ab0
Gert Cuykens <gert.cuykens@gmail.com>
2011-10-02 22:11:55 +0200

    xhr

--- users/_design/user/http.js
+++ users/_design/user/http.js
@@ -1,23 +1,13 @@
 // Copyright(c) gert.cuykens@gmail.com
 xhr=new XMLHttpRequest()
+xhr.addEventListener('loadstart',function(e){statBox.style.display='inline-block'},false)
+xhr.addEventListener('loadstart',function(e){statBox.style.backgroundColor='green'},false)
+xhr.addEventListener('loadstart',function(e){statBox.value=0},false)
 xhr.addEventListener('abort',function(e){statBox.style.display='none'},false)
+xhr.addEventListener('progress',function(e){if(e.lengthComputable)statBox.value=Math.round((e.loaded*100)/e.total)},false)
 xhr.addEventListener('error',function(e){statBox.style.backgroundColor='red'},false)
-xhr.addEventListener('readystatechange',function(e){
- switch(xhr.readyState){
-  case 1:
-   statBox.style.display='inline-block'
-   statBox.style.backgroundColor='green'
-   statBox.value=0
-  break;
-  case 3:
-   if(e.lengthComputable)statBox.value=Math.round((e.loaded*100)/e.total)
-  break;
-  case 4:
-   ETag=xhr.getResponseHeader('ETag')
-   statBox.style.display='none'
-  break;
- }
-},false)
+xhr.addEventListener('load',function(e){ETag=xhr.getResponseHeader('ETag')},false)
+xhr.addEventListener('load',function(e){statBox.style.display='none'},false)
 
 preview=function(f,d){
  if (!f.type.match(/image.*/))return false
